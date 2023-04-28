1de5635bc3cddf8150b180e4ed6ffd4761d7d6b6
bogeychan <bogeychan@outlook.com>
2023-04-28 22:50:56 +0200

    fix: upgrade `elysia-polyfills` to 0.0.6

--- template-deno/deps.ts
+++ template-deno/deps.ts
@@ -1,4 +1,4 @@
-import 'npm:@bogeychan/elysia-polyfills@0.0.4/deno/index.js';
+import 'npm:@bogeychan/elysia-polyfills@0.0.6/deno/index.js';
 import 'npm:@sinclair/typebox@0.26.0'; // deno doesn't download peerDependencies. this one is required
 
 export { Elysia } from 'npm:elysia@0.4.9';
