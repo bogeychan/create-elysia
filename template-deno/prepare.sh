8ee5f2f69b42696b707dfb329b8f1812abd9fc06
bogeychan <bogeychan@outlook.com>
2023-05-01 18:28:31 +0200

    fix: always use latest `elysia-polyfills`

--- template-deno/prepare.sh
+++ template-deno/prepare.sh
@@ -6,7 +6,7 @@ rm -fr node_modules deno.lock
 echo "Downloading..."
 
 # Try to run (this shall fail & create a `node_modules` folder)
-deno run --node-modules-dir deps.ts &> /dev/null
+deno run --reload=npm:@bogeychan/elysia-polyfills --node-modules-dir deps.ts &> /dev/null
 
 # Transpile a few `node_modules` packages to conform ESM
 ./node_modules/@bogeychan/elysia-polyfills/bin/cli.js
